0edd6835ea92d66bf22da9b5034f9f2840668372
jazattack <150624319+jazattack@users.noreply.github.com>
2023-11-28 19:09:33 -0500

    enter info

--- spotify data scraping/config.py
+++ spotify data scraping/config.py
@@ -1,5 +1,5 @@
-client_id = # enter your client id
-client_secret = #  enter your client secret
-username = # enter your username
+client_id = fdd0993c9a604daeaa820a5195e0da90
+client_secret = e390ac5703574966aa1af161d73c5fe4
+username = 1237112444
 redirect_uri = 'http://localhost:7777/callback'
 scope = 'user-read-recently-played'
